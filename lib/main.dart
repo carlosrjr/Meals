ab520ef8616515e99983024b02e43d10b2e016fe
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2020-03-11 17:11:12 -0300

    [ADD] Adicionando listagem de categorias das refeiçoes

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+          ),
+        ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
