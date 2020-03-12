f5a6331e9d89a368d0ff8405ff3cb522256bce5a
Carlos Roberto Barbosa Júnior <carlosjr.if@gmail.com>
2020-03-12 13:02:17 -0300

    [ADD] Adicionando listagem dos pratos por categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/components/meal_item.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-  final category = ModalRoute.of(context).settings.arguments as Category;
+    final category = ModalRoute.of(context).settings.arguments as Category;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
         backgroundColor: category.color,
       ),
-      body: Center(
-        child: Text('Receitas por categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
       ),
     );
   }
